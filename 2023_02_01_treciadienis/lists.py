9c2eaa0bb6da863d4d7a6d1073ad73080a5b4fd8
kraujinis <kraujinis@gmail.com>
2023-02-01 20:15:35 +0200

    20:15

--- 2023_02_01_treciadienis/lists.py
+++ 2023_02_01_treciadienis/lists.py
@@ -4,9 +4,9 @@ my_list = [1, 2, 5, 6, 8, 5, 5]
 new_list = []
 
 for i in my_list:
-    i += 50
-    new_list.append(i)
+    i += 50 # prie my_list kiekvieno elemento pridedama 50
+    new_list.append(i)  # i reikšmės įrašomos į naują sąrašą
 
 
-print(my_list)
-print(new_list)
+print(my_list)  # senas sąrašas
+print(new_list) # naujas sąrašas
