30d187b0c8161fa2096c8b1d26b55be6121b3eba
kraujinis <kraujinis@gmail.com>
2023-02-09 21:32:37 +0200

    2023 02 09 21:32

--- Lesson_8/exercise_2 .py
+++ Lesson_8/exercise_2 .py
@@ -3,4 +3,20 @@
 # Then count how many each letters are in those words.
 # Return answer as a dictionary. {'letter': count}
 # And all words sorted in one list.
+import random
+
+letter_list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+                 'l', 'm', 'n', 'o', 'p', 'q', 'r']
+
+letter_list_1 = []
+letter_list_2 = []
+
+for _ in range(0, 10):
+    for _ in range(0, 5):
+        a = random.choice(letter_list)
+        letter_list_1.append(a)
+print(letter_list_1)      
+print(letter_list_2)
+
+
 
