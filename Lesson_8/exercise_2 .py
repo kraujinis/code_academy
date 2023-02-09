49d4e6227ece8b6b07a609a7d4cd5cac413007c7
kraujinis <kraujinis@gmail.com>
2023-02-09 20:15:38 +0200

    2023 02 07 20:15

---
+++ Lesson_8/exercise_2 .py
@@ -0,0 +1,6 @@
+# Create a list of letters and generate 5 diferent words for 5 different lists.
+# (A word must the size between 5 and letters)
+# Then count how many each letters are in those words.
+# Return answer as a dictionary. {'letter': count}
+# And all words sorted in one list.
+
