f9c66bf1eccddc7fbbc566b30c1fc451e9b42ba5
kraujinis <kraujinis@gmail.com>
2023-02-11 12:59:42 +0200

    2023_02_11

--- Lesson_8/home_work.py
+++ Lesson_8/home_work.py
@@ -3,10 +3,17 @@
 # Then count how many each letters are in those words.
 # Return answer as a dictionary. {'letter': count}
 # And all words sorted in one list.
+
 import random
 
-letter_list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
-                 'l', 'm', 'n', 'o', 'p', 'q', 'r']
+# START 1 --> create 5 lists with diferent lenght of 5 words
+
+letter_list = [
+                'a', 'b', 'c', 'd', 'e', 'f',
+                'g', 'h', 'i', 'j', 'k', 'l',
+                'm', 'n', 'o', 'p', 'q', 'r'
+                ]
+
 
 letter_list_1 = []
 letter_list_2 = []
@@ -14,47 +21,34 @@ letter_list_3 = []
 letter_list_4 = []
 letter_list_5 = []
 
-for _ in range(0, 5):
-    for _ in range(0, 5):
-        a = random.sample(letter_list, 5)
-    b = ''.join(a)
-    letter_list_1.append(b)
+for _ in range(0, 5):   # create first loop of 5 for make 5 words in list
+    letter_random = random.sample(letter_list, random.randint(5, 15))   # take random 5 letters from list
+    letter_list_1.append(''.join(letter_random))   # join and append letters to list
 
-    if len(letter_list_1) >= 5:
+    if len(letter_list_1) >= 5:   # checking if list lenght is 5
         for _ in range(0, 5):
-            for _ in range(0, 5):
-                a = random.sample(letter_list, 5)
-            b = ''.join(a)
-            letter_list_2.append(b)
+            letter_random = random.sample(letter_list, random.randint(5, 15))
+            letter_list_2.append(''.join(letter_random))
 
         if len(letter_list_2) >= 5:
             for _ in range(0, 5):
-                for _ in range(0, 5):
-                    a = random.sample(letter_list, 5)
-                b = ''.join(a)
-                letter_list_3.append(b)
+                letter_random = random.sample(letter_list, random.randint(5, 15))
+                letter_list_3.append(''.join(letter_random))
 
             if len(letter_list_3) >= 5:
                 for _ in range(0, 5):
-                    for _ in range(0, 5):
-                        a = random.sample(letter_list, 5)
-                    b = ''.join(a)
-                    letter_list_4.append(b)
+                    letter_random = random.sample(letter_list, random.randint(5, 15))
+                    letter_list_4.append(''.join(letter_random))
 
                 if len(letter_list_4) >= 5:
                     for _ in range(0, 5):
-                        for _ in range(0, 5):
-                            a = random.sample(letter_list, 5)
-                        b = ''.join(a)
-                        letter_list_5.append(b)
+                        letter_random = random.sample(letter_list, random.randint(5, 15))
+                        letter_list_5.append(''.join(letter_random))
+
+# --> END 1
+
+# START 2 --> then count how many each letters are in those words.
+# Return answer as a dictionary. {'letter': count}
 
 
-
-
-print(letter_list_1)      
-print(letter_list_2)
-print(letter_list_3)
-print(letter_list_4)
-print(letter_list_5)
-
-
+# --> END 2
