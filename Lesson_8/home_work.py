5a5f0e975bace662f06c51c79fae7392e31c9d6a
kraujinis <kraujinis@gmail.com>
2023-02-11 09:55:23 +0200

    2023_02_11

--- Lesson_8/home_work.py
+++ Lesson_8/home_work.py
@@ -10,13 +10,53 @@ letter_list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
 
 letter_list_1 = []
 letter_list_2 = []
+letter_list_3 = []
+letter_list_4 = []
+letter_list_5 = []
 
-for _ in range(0, 10):
+for _ in range(0, 5):
     for _ in range(0, 5):
-        a = random.choice(letter_list)
-        letter_list_1.append(a)
+        a = random.sample(letter_list, 5)
+    b = ''.join(a)
+    letter_list_1.append(b)
+    if len(letter_list_1) >= 5:
+        for _ in range(0, 5):
+            for _ in range(0, 5):
+                a = random.sample(letter_list, 5)
+            b = ''.join(a)
+            letter_list_2.append(b)
+        print(len(letter_list_2))
+
+        if len(letter_list_2) >= 5:
+            for _ in range(0, 5):
+                for _ in range(0, 5):
+                    a = random.sample(letter_list, 5)
+                b = ''.join(a)
+                letter_list_3.append(b)
+            print(len(letter_list_3))
+
+            if len(letter_list_3) >= 5:
+                for _ in range(0, 5):
+                    for _ in range(0, 5):
+                        a = random.sample(letter_list, 5)
+                    b = ''.join(a)
+                    letter_list_4.append(b)
+                print(len(letter_list_3))
+
+                if len(letter_list_4) >= 5:
+                    for _ in range(0, 5):
+                        for _ in range(0, 5):
+                            a = random.sample(letter_list, 5)
+                        b = ''.join(a)
+                        letter_list_5.append(b)
+                    print(len(letter_list_3))
+
+
+
 print(letter_list_1)      
 print(letter_list_2)
-
+print(letter_list_3)
+print(letter_list_4)
+print(letter_list_5)
 
 
