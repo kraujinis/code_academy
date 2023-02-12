48555b2e3bc1db40d29a9b09f4e7825b9be1de0e
kraujinis <kraujinis@gmail.com>
2023-02-12 19:33:44 +0200

    2023_02_12

--- Lesson_8/home_work.py
+++ Lesson_8/home_work.py
@@ -21,29 +21,32 @@ letter_list_4 = []
 letter_list_5 = []
 unique_letter_dictionary = {}
 
-for _ in range(0, 5):   # create first loop of 5 for make 5 words in list
-    letter_random = random.sample(letter_list, random.randint(5, 15))   # take random 5 letters from list
-    letter_list_1.append(''.join(letter_random))   # join and append letters to list
+'''
+Create 5 lists with diferent lenght of 5 words
+    1) Loop for of 5, create 5 lists
+        2) Take a random letter ramdomly from 5-15 letter from {letter_list} and join to word
+        
+        3) If the length of lists items is 5 make list and go to create next list
+'''
 
-    if len(letter_list_1) >= 5:   # checking if list lenght is 5
+for _ in range(0, 5):
+    letter_list_1.append(''.join(random.sample(letter_list, random.randint(5, 15))))
+
+    if len(letter_list_1) >= 5:
         for _ in range(0, 5):
-            letter_random = random.sample(letter_list, random.randint(5, 15))
-            letter_list_2.append(''.join(letter_random))
+            letter_list_2.append(''.join(random.sample(letter_list, random.randint(5, 15))))
 
         if len(letter_list_2) >= 5:
             for _ in range(0, 5):
-                letter_random = random.sample(letter_list, random.randint(5, 15))
-                letter_list_3.append(''.join(letter_random))
+                letter_list_3.append(''.join(random.sample(letter_list, random.randint(5, 15))))
 
             if len(letter_list_3) >= 5:
                 for _ in range(0, 5):
-                    letter_random = random.sample(letter_list, random.randint(5, 15))
-                    letter_list_4.append(''.join(letter_random))
+                    letter_list_4.append(''.join(random.sample(letter_list, random.randint(5, 15))))
 
                 if len(letter_list_4) >= 5:
                     for _ in range(0, 5):
-                        letter_random = random.sample(letter_list, random.randint(5, 15))
-                        letter_list_5.append(''.join(letter_random))
+                        letter_list_5.append(''.join(random.sample(letter_list, random.randint(5, 15))))
 
 # START 2 --> then count how many each letters are in those words.
 # Return answer as a dictionary. {'letter': count}
@@ -52,15 +55,19 @@ all_words_list_joined = ''.join(letter_list_1) + ''.join(letter_list_2) + ''.joi
 
 all_words_list = letter_list_1 + letter_list_2 + letter_list_3 + letter_list_4 + letter_list_5
 
-#print(" List all words joined: ", all_words_list_joined)
+# print(" List all words joined: ", all_words_list_joined)
 
 unique_letter_set = ''.join(set(all_words_list_joined))
 
-#print("Unique letter set: ", unique_letter_set)
+# print("Unique letter set: ", unique_letter_set)
 
-# extract unique letters and count them
+"""
+Create a dictionary:
+    1) Take one letter {letter_x} from joined {unique_letter_set}
+    2) Count how many times that letter appears in {all_words_list_joined}
+"""
 for letter_x in unique_letter_set:
-    unique_letter_dictionary[letter_x] = all_words_list_joined.count(letter_x)   # create dictionary 
+    unique_letter_dictionary[letter_x] = all_words_list_joined.count(letter_x)
 
 print("Counted letter dictionary: ", unique_letter_dictionary)
 print("Sum of all letter in lists: ", sum(unique_letter_dictionary.values()))
