a17a2af6d1f5f4e95d738fc7c5794a1b900a04f4
kraujinis <kraujinis@gmail.com>
2023-02-15 22:09:39 +0200

    2023 02 15

--- Lesson_9/exercise_3.py
+++
@@ -1,4 +0,0 @@
-list_names = ["Giedrius", "Antanas", "Darius", "Marius", "Bob", "Johannes"]
-
-names_dictionary = {keys: values for keys, values in enumerate(list_names)}
-print(names_dictionary)
---
+++ lesson_11/exercise_3.py
@@ -0,0 +1,16 @@
+
+list_1 = ["at", "be", "th", "au"]
+list_2 = ["beautiful", "the", "hat"]
+
+
+def bigram(item1: list[str], item_2: list[str]) -> bool:
+    gg = []
+    gk = []
+    for i in item_2:
+        gg.append(i)
+    
+        
+    print(gg)
+
+
+print(bigram(list_1, list_2))
