6b45d62f0663ab5848b80647736f2edfed789d7d
kraujinis <kraujinis@gmail.com>
2023-02-17 21:40:10 +0200

    2023 02 17

--- lesson_11/exercise_3.py
+++ lesson_11/exercise_3.py
@@ -1,16 +1,38 @@
 
-list_1 = ["at", "be", "th", "au"]
-list_2 = ["beautiful", "the", "hat"]
+from typing import List
+
+# list_1 = ["at", "be", "th", "au"]
+# list_2 = ["beautiful", "the", "hat"]    # True
+
+# list_1 = ["ay", "be", "ta", "cu"]
+# list_2 = ["maybe", "beta", "abet", "course"]    # False
+
+list_1 = ["th", "fo", "ma", "or"]
+list_2 = ["the", "many", "for", "forest"]   # True
+
+# list_1 = ["oo", "mi", "ki", "la"]
+# list_2 = ["milk", "chocolate", "cooks", "cooks"]    # False
 
 
-def bigram(item1: list[str], item_2: list[str]) -> bool:
-    gg = []
-    gk = []
-    for i in item_2:
-        gg.append(i)
-    
-        
-    print(gg)
+def bigram(list1: List[str], list2: List[str]) -> bool:
+
+    list_double = []
+    filtered = []
+
+    for i in range(len(list2)):
+        for j in range(len(list2[i]) - 1):
+            a = list2[i][j]
+            b = list2[i][j + 1]
+            c = a + b
+            list_double.append(c)
+
+    for i in list1:
+        if i in list_double:
+            filtered.append(i)
+    if len(list1) == len(filtered):
+        return True
+    else:
+        return False
 
 
 print(bigram(list_1, list_2))
