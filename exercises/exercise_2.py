5718a0ba912569f4c240d9c91d6949a9627f823a
kraujinis <kraujinis@gmail.com>
2023-03-13 18:25:13 +0200

    2023

---
+++ exercises/exercise_2.py
@@ -0,0 +1,15 @@
+# Ideas:
+# -----------------------------------
+# Kalkuliatorius 
+# Arabiškų skaičių konvertavimas į romėniškus ir atvirkščiai 
+# Boulingo taškų skaičiuoklė 
+# Saugių slaptažodžių generatorius 
+# Klausimynas 
+# Testas 
+# Žaidimai: Kryžiukai/nuliukai,  Akmuo, lapas, žirklės 
+# Bet kokia kita norima programa. 
+# ------------------------------------
+# Requirements: OOP (Inheritance as minimum), functions, modules, import, types, clean code , naming, logging.
+
+# + venv, github
+# + Error handling
